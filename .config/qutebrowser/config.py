ab930991961db5d7741f46516d2c23de7b1264d2
Nathan Blair <nathan.t.blair@outlook.com>
2018-12-20 01:34:55 -0500

    Updating qutebrowser settings

---
+++ .config/qutebrowser/config.py
@@ -0,0 +1,14 @@
+import dracula.draw
+
+config.load_autoconfig()
+
+dracula.draw.blood(c, {
+    'spacing': {
+        'vertical': 6,
+        'horizontal': 8
+        },
+    'font': {
+        'family': 'Hack, Menlo, Terminus, Monospace',
+        'size': 14
+        }
+    })
