0c4d7f3812cb7503c5ab320153debade98cbae60
Nathan Blair <engineer.nblair@gmail.com>
2021-09-01 06:03:59 -0400

    removing vscodium settings apparently
    and mildly better pwsh profile

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -42,24 +42,63 @@ function gsc ($branch) { git switch -c $branch }
 function gpush ($arguments) { git push $arguments }
 function gpull { git pull }
 
+# Docker function commands
+function disa { docker images -a }
+function dcrit ($arguments) { docker run --rm -it $arguments }
+
 # Terraform alias
 Set-Alias -Name tf -Value terraform
 
 function Prompt {
+    $last_exit_code = 0
+    if (Test-Path variable:global:LASTEXITCODE) {
+        $last_exit_code = $LASTEXITCODE
+    }
+
     $branch = git branch --show-current
 
     if ($LASTEXITCODE -eq 0) {
         $formatted_root_path = $(git rev-parse --show-toplevel) -replace '/', '\'
-        Write-Host $($(Get-Location) -replace [Regex]::Escape($formatted_root_path), $(Get-Item $formatted_root_path).Name) -ForegroundColor DarkCyan -NoNewline
+        Write-Host $(
+            $(Get-Location) -replace [Regex]::Escape($formatted_root_path),
+            $(Get-Item $formatted_root_path).Name
+        ) -ForegroundColor Cyan -NoNewline
         Write-Host " (" -ForegroundColor Blue -NoNewline
         Write-Host "$branch" -ForegroundColor Magenta -NoNewline
         Write-Host ")" -ForegroundColor Blue -NoNewline
     }
     else {
-        Write-Host $($(Get-Location) -replace [Regex]::Escape($HOME)), '~' -ForegroundColor DarkCyan -NoNewline
+        Write-Host $(
+            $(Get-Location) -replace [Regex]::Escape($HOME)
+        ), '~' -ForegroundColor DarkCyan -NoNewline
     }
 
-    Write-Host (' >') -ForegroundColor Green -NoNewline
+    if ($last_exit_code -ne 0) {
+        Write-Host " ($last_exit_code)" -ForegroundColor Blue -NoNewline
+        Write-Host (' >') -ForegroundColor Red -NoNewline
+    }
+    else {
+        Write-Host (' >') -ForegroundColor Green -NoNewline
+    }
+
+    $last_command = Get-History -Count 1
+    $right_message = ($last_command.EndExecutionTime - $last_command.StartExecutionTime).Seconds
+    $extra_character_length = 2
+
+    $window_host = $(Get-Host)
+    $original_position = $window_host.UI.RawUI.CursorPosition
+    $new_position = $original_position
+
+    $buffer_size = $window_host.UI.RawUI.BufferSize
+    $new_position.X = $buffer_size.Width - ("${right_message}".Length + $extra_character_length)
+
+    $window_host.UI.RawUI.CursorPosition = $new_position
+    Write-Host "[" -ForegroundColor Blue -NoNewline
+    Write-Host $right_message -ForegroundColor DarkGray -NoNewline
+    Write-Host "]" -ForegroundColor Blue -NoNewline
+    $window_host.UI.RawUI.CursorPosition = $original_position
+
+    $LASTEXITCODE = $last_exit_code
     return " "
 }
 
@@ -84,6 +123,8 @@ $env:PATH = "$env:HOMEPATH/.local/bin; $env:PATH"
 
 # Source token variables
 . ~/repos/personal/keys/tokens.ps1 | Out-Null
-New-Item -Name PIPENV_VENV_IN_PROJECT -Path env: -ItemType Variable -Value 1 -ErrorAction SilentlyContinue | Out-Null
+if (-Not (Test-Path env:PIPENV_VENV_IN_PROJECT)) {
+    New-Item -Name PIPENV_VENV_IN_PROJECT -Path env: -ItemType Variable -Value 1 | Out-Null
+}
 
 # Import-Module posh-git
