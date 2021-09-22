56d582d98d882d8c1f411420a4e23839fcd83e35
Nathan Blair <engineer.nblair@gmail.com>
2021-09-22 00:21:10 -0400

    exporting GH_USERNAME for pwsh

--- .env.ps1
+++ .env.ps1
@@ -1,4 +1,5 @@
 $env:PATH = "$env:HOMEPATH/.local/bin; $env:PATH"
+$env:GH_USERNAME = "nathanblair"
 
 if (-Not (Test-Path env:PIPENV_VENV_IN_PROJECT)) {
     New-Item -Name PIPENV_VENV_IN_PROJECT -Path env: -ItemType Variable -Value 1 | Out-Null
