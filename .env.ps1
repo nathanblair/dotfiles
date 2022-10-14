ef64f8e4c1d00cb223948fe6640c009f1c41aac1
Nathan Blair <me@nathanblair.rocks>
2022-10-14 05:56:47 -0700

    include p4config in ps1 env

--- .env.ps1
+++ .env.ps1
@@ -1,6 +1,3 @@
 $env:PATH = "$env:HOMEPATH/.local/bin; $env:PATH"
 $env:GH_USERNAME = "nathanblair"
-
-if (-Not (Test-Path env:PIPENV_VENV_IN_PROJECT)) {
-    New-Item -Name PIPENV_VENV_IN_PROJECT -Path env: -ItemType Variable -Value 1 | Out-Null
-}
+$env:P4CONFIG = ".p4config"
